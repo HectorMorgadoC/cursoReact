bc4a172c9154efde29a4c3faf5634bae9350cca6
Hector Morgado <e3.corest@outlook.com>
2024-08-04 11:39:25 -0500

    se modifica el archivo pricipal de ecommerce

--- cursoReact/Ecommerce/encommerce/src/main.jsx
+++ cursoReact/Ecommerce/encommerce/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { FiltersProvider } from './context/Filters.jsx'
 
 
-// en vez de usar el React.StrictMode se usa el useContext creado en este caso FiltersContext.Provider 
+// en vez de usar el React.StrictMode se usa el useContext creado en este caso FiltersProvider 
 ReactDOM.createRoot(document.getElementById('root')).render(
  < FiltersProvider>
    <App />
